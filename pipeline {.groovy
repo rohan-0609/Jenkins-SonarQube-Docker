828edaa62e8bbfec8e4cd30058643678cff20210
Rohan Raj Aggarwal <58262424+rohan-0609@users.noreply.github.com>
2024-10-05 03:20:30 -0500

    Add files via upload

---
+++ pipeline {.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+    tools {
+        maven "MAVEN3"
+        jdk "OracleJDK11"
+    }
+
+    stages {
+        stage('Fetch Code'){
+            steps{
+                git branch: 'main', url: 'https://github.com/bardsoftware/biz.ganttproject.app.localization.git'
+            }
+        }
+    }
+
+    stage('Build'){
+        steps{
+            sh 'mvn install -DskipTests'
+        }
+        post{
+            success {
+                echo 'Archiving artifacts now !!'
+                archiveArtifacts artifact: '**/*.war'
+            }
+        }
+    }
+
+    stage('UNIT TEST'){
+        steps{
+            sh 'mvn test'
+         }
+    }
+}
